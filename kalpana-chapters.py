6c21c0ffeb6613a031006a976c55055ee4a9fe52
nycz <pyntix@gmail.com>
2013-08-30 20:17:42 +0200

    The ugly colors have been dealt with. Fixes #2
    Now the active chapter is marked with bold text, not color.

--- kalpana-chapters.py
+++ kalpana-chapters.py
@@ -6,7 +6,7 @@ import shutil
 
 from PyQt4 import QtCore, QtGui
 
-from libsyntyche.common import read_json
+from libsyntyche.common import read_json, parse_stylesheet, read_file
 from pluginlib import GUIPlugin
 
 
@@ -26,6 +26,11 @@ class UserPlugin(GUIPlugin):
             shutil.copyfile(os.path.join(self.pluginpath, 'defaultconfig.json'),
                             configfile)
         self.sidebar.settings = read_json(configfile)
+        cssfile = os.path.join(self.configpath, 'kalpana-chapters.css')
+        if not os.path.exists(cssfile):
+            shutil.copyfile(os.path.join(self.pluginpath, 'defaulttheme.css'),
+                            cssfile)
+        self.sidebar.setStyleSheet(parse_stylesheet(read_file(cssfile)))
 
 
 class Sidebar(QtGui.QListWidget):
@@ -76,25 +81,36 @@ class Sidebar(QtGui.QListWidget):
                 self.linenumbers = [0] + list(self.linenumbers)
                 items.insert(0, prefix + '\n   ' + str(wc))
         self.addItems(items)
+        self.mod_items_fonts(bold=True)
         self.setFixedWidth(self.sizeHintForColumn(0)+5)
+        self.mod_items_fonts(bold=False)
+        self.item(0).setFont(mod_font(self.item(0), bold=True))
         self.show()
 
     def update_active_chapter(self):
         if not self.count() or not self.isVisible():
             return
-        if not self.default_item_bg:
-            self.default_item_bg = self.item(0).backgroundColor()
         pos = self.textarea.textCursor().blockNumber()+1
-        for item_nr in range(self.count()):
-            self.item(item_nr).setBackgroundColor(self.default_item_bg)
+        self.mod_items_fonts(bold=False)
         for n, ch in list(enumerate(self.linenumbers))[::-1]:
             if pos >= ch:
-                self.item(n).setBackgroundColor(QtCore.Qt.darkGreen)
+                i = self.item(n)
+                i.setFont(mod_font(i, True))
                 break
 
     def goto_chapter(self, _):
         self.textarea.goto_line(self.linenumbers[self.currentRow()])
 
+    def mod_items_fonts(self, bold):
+        for item_nr in range(self.count()):
+            i = self.item(item_nr)
+            i.setFont(mod_font(i, bold))
+
+
+def mod_font(item, bold):
+    font = item.font()
+    font.setBold(bold)
+    return font
 
 def get_chapter_wordcounts(real_chapter_lines, text):
     chapter_lines = list(real_chapter_lines) + [len(text)]
